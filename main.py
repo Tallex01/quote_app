215dfdca392467ce501a889feddaedb9066a37ca
Alex Goodman <79115163+Tallex01@users.noreply.github.com>
2026-05-19 00:47:59 +0000

    set up endpoint

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
+import pandas as pd
+import random
 
 app = FastAPI()
 
+data = pd.read_csv("quotes.csv")
+
+@app.get("/get_quote")
+def get_quotes():
+    pairs = {"Quote": data["text"], "Author": data[author]}
+    return pairs
 
 
 
